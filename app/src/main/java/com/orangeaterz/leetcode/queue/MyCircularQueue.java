370df7596a3043466502b9eb3b88d08be8782494
xiongzhuang <xiongzhuang@dcoin.com>
2018-11-13 19:04:36 +0800

    modify MyCircularQueue

--- app/src/main/java/com/orangeaterz/leetcode/queue/MyCircularQueue.java
+++ app/src/main/java/com/orangeaterz/leetcode/queue/MyCircularQueue.java
@@ -1,68 +1,96 @@
 package com.orangeaterz.leetcode.queue;
 
 
-public class MyCircularQueue {
+class MyCircularQueue {
+
     private int[] data;
-    private int tail;
     private int head;
-    private int count;
-    private int maxLength;
+    private int tail;
+    private int size;
 
-
+    /**
+     * Initialize your data structure here. Set the size of the queue to be k.
+     */
     public MyCircularQueue(int k) {
         data = new int[k];
-        maxLength = k;
-        tail = 0;
-        head = 0;
-        count = 0;
+        head = -1;
+        tail = -1;
+        size = k;
     }
 
+    /**
+     * Insert an element into the circular queue. Return true if the operation is successful.
+     */
     public boolean enQueue(int value) {
-        if (isFull()) {
+        if (isFull() == true) {
             return false;
-        } else {
-            data[tail] = value;
-            count++;
-            tail = (tail + 1) / maxLength;
-            return true;
         }
-
+        if (isEmpty() == true) {
+            head = 0;
+        }
+        tail = (tail + 1) % size;
+        data[tail] = value;
+        return true;
     }
 
+    /**
+     * Delete an element from the circular queue. Return true if the operation is successful.
+     */
     public boolean deQueue() {
-        if (isEmpty()) {
+        if (isEmpty() == true) {
             return false;
-        } else {
-            count--;
-            head = (head + 1) / maxLength;
+        }
+        if (head == tail) {
+            head = -1;
+            tail = -1;
             return true;
         }
+        head = (head + 1) % size;
+        return true;
     }
 
+    /**
+     * Get the front item from the queue.
+     */
     public int Front() {
-        if (isEmpty()) {
+        if (isEmpty() == true) {
             return -1;
-        } else {
-            return data[head];
         }
-
+        return data[head];
     }
 
+    /**
+     * Get the last item from the queue.
+     */
     public int Rear() {
-        if (isEmpty()) {
+        if (isEmpty() == true) {
             return -1;
-        } else {
-            return data[tail];
         }
+        return data[tail];
     }
 
+    /**
+     * Checks whether the circular queue is empty or not.
+     */
     public boolean isEmpty() {
-        return count == 0;
+        return head == -1;
     }
 
+    /**
+     * Checks whether the circular queue is full or not.
+     */
     public boolean isFull() {
-        return count == maxLength;
+        return ((tail + 1) % size) == head;
     }
-
-
 }
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue obj = new MyCircularQueue(k);
+ * boolean param_1 = obj.enQueue(value);
+ * boolean param_2 = obj.deQueue();
+ * int param_3 = obj.Front();
+ * int param_4 = obj.Rear();
+ * boolean param_5 = obj.isEmpty();
+ * boolean param_6 = obj.isFull();
+ */
